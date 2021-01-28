279acfe8b7e32e38f539e327224f7e62d074161b
Helia Fotoohi <heliafotoohi@Helias-MacBook.attlocal.net>
2021-01-27 22:46:48 -0800

    booool

---
+++ lab5/js/lab.js
@@ -0,0 +1,16 @@
+/*
+ * Author: Helia Fotoohi
+
+
+
+
+
+make = "Nissan"
+model = "Sentra"
+Color = "White"
+year = 2006
+age = 2020 - 2006
+var = mycar
+var = "mycar"
+var ownIt = true
+console.log("age: " + age)
