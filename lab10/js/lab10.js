80ca94e38cc9a0c0eeee56ff837c2dc404826222
Helia Fotoohi <heliafotoohi@Helias-MacBook.local>
2021-03-15 15:36:20 -0700

    hi

--- lab10/js/lab10.js
+++ lab10/js/lab10.js
@@ -4,45 +4,20 @@
 *
 **/
 
-var newName = sortUserName(userName);
-var userName = document.getElementById("name");
+var sortButton = document.getElementById("sort-button");
 
-// sortUserName - a function that takes user input and sorts the letters
-// of their name
-function sortUserName(userName) {
-  // console.log('ALPHABET'.toLowerCase()); // 'alphabet'
-   userName = userName.toLowerCase();
-    console.log("userName =", userName);
-    var nameArray = userName.split('');
-    console.log("nameArray =", nameArray);
-    varnameArraySort = nameArray.sort();
-    console.log("nameArraySort =", nameArraySort);
-    var nameSorted = nameArraySort.join('');
-    console.log("nameSorted =", nameSorted);
-    return userName.toLowerCase().split("").sort().join("");
-}
+//event listener
+sortButton.addEventListener("click", function(){
+  // find elements
+  var output = document.getElementById("output");
+  var name = document.getElementById("user-name");
+  var userName = name.value;
 
-document.writeln("This is what you wrote down: ", userName, "<br><br>");
-document.writeln("And here's your new name made just for you: ",
-     newName, "</br>");
+  //sort name
+  var sortedName = userName.toUpperCase().replace(/\s+/g, '').split('').sort().join('');
 
-
-
-
-//function sortUserName(userName) {
-    // // convert to lower case
-    // userName = userName.toLowerCase();
-    // console.log("userName =", userName);
-    // // split string to array
-    // var nameArray = userName.split('');
-    // console.log("nameArray =", nameArray);
-    // // sort the array
-    // var nameArraySort = nameArray.sort();
-    // console.log("nameArraySort =", nameArraySort);
-    // // join array back to a string
-    // var nameSorted = nameArraySort.join('');
-    // console.log("nameSorted =", nameSorted);
-    // // Note that I could have done the above lines as a single line:
-    // //   userName.toLowerCase().split("").sort().join("")
-    //return userName.toLowerCase().split("").sort().join("");
-//}
+  //writes output to page
+  var newEl = document.createElement("p");
+  newEl.innerHTML = sortedName;
+  output.append(newEl);
+});
